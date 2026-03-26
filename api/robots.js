b96c4383880da5ddc475d144594ee4a217c5f951
KSA Verified Admin <cupido1romeo@gmail.com>
2026-03-26 05:50:47 +0300

    chore: Remove redundant SEO API files

--- api/robots.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-    const robots = `User-agent: *
-Allow: /
-Sitemap: https://ksaverified.com/sitemap.xml
-`;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.status(200).send(robots);
-};
